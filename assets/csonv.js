c67dcbb8fa543618f78b5e3930df16d8570e4b34
Paul Engel <paulengel@PowahBook.local>
2011-06-12 03:13:11 +0200

    Updated csonv.js

--- assets/csonv.js
+++ assets/csonv.js
@@ -8,7 +8,7 @@ if (typeof(Csonv) == "undefined") {
 // * Except otherwise noted, csonv.js is licensed under
 // * http://creativecommons.org/licenses/by-sa/3.0
 // *
-// * $Date: 2011-06-11 20:56:43 +0100 (Sat, 11 June 2011) $
+// * $Date: 2011-06-12 03:07:27 +0100 (Sun, 12 June 2011) $
 // *
 
 Csonv = (function() {
@@ -16,12 +16,12 @@ Csonv = (function() {
 
   var defineParseMethods = function() {
     var arrayOf = function(type, values) {
-      var strings = values.splitCsv(",");
-      var result  = [];
+      var strings = values.csvSplit(",");
+      var array   = [];
       for (var i = 0; i < strings.length; i++) {
-        result.push(parseMethods[type](strings[i]));
+        array.push(parseMethods[type](strings[i]));
       }
-      return result;
+      return array;
     };
 
     parseMethods = {
@@ -66,53 +66,59 @@ Csonv = (function() {
 
   var csvToObjects = function(csv) {
     var rows  = csv.split("\n");
-    var keys  = rows.shift().splitCsv();
-    var types = rows.shift().splitCsv();
+    var keys  = rows.shift().csvSplit();
+    var types = rows.shift().csvSplit();
 
     var methods = [];
     for (var i = 0; i < types.length; i++) {
       methods.push(parseMethods[types[i]]);
     }
 
-    var result = [];
+    var array = [];
     for (var i = 0; i < rows.length; i++) {
-      var row    = rows[i].splitCsv();
-      var object = {};
+      var row = rows[i].csvSplit(), object = {};
       for (var j = 0; j < keys.length; j++) {
         object[keys[j]] = methods[j](row[j]);
       }
-      result.push(object);
+      array.push(object);
     }
 
-    return result;
+    return array;
   };
 
   return {
     version: "0.1.0",
-    sep: ";",
-    init: function() {
-      defineParseMethods();
-      if (typeof(onCsonvReady) == "function") {
-        onCsonvReady();
-      };
-    },
-    fetch: fetch
+    sep    : ";",
+    init   : defineParseMethods,
+    fetch  : fetch
   };
 }());
 
 String.prototype.toData = function() {
-  return Csonv.fetch(this.toString()); };
+  return Csonv.fetch(this);
+};
 
-String.prototype.splitCsv = function(sep) {
-  for (var foo = this.split(sep = sep || Csonv.sep), x = foo.length - 1, tl; x >= 0; x--) {
-    if (foo[x].replace(/"\s+$/, '"').charAt(foo[x].length - 1) == '"') {
-      if ((tl = foo[x].replace(/^\s+"/, '"')).length > 1 && tl.charAt(0) == '"') {
-        foo[x] = foo[x].replace(/^\s*"|"\s*$/g, '').replace(/""/g, '"');
-      } else if (x) {
-        foo.splice(x - 1, 2, [foo[x - 1], foo[x]].join(sep));
-      } else foo = foo.shift().split(sep).concat(foo);
-    } else foo[x].replace(/""/g, '"');
-  } return foo; };
+String.prototype.csvSplit = function(s) {
+  s = s || Csonv.sep;
+
+  var reg_exp = new RegExp(("(\\" + s + '|\\r?\\n|\\r|^)(?:"([^"]*(?:""[^"]*)*)"|([^"\\' + s + "\\r\\n]*))"), "gi");
+  var row = [], m = null;
+
+  if (this.match(new RegExp("^\\" + s))) {
+    row.push("");
+  }
+
+  while (m = reg_exp.exec(this)) {
+    var m1 = m[1];
+    if (m1.length && (m1 != s)) {
+      row.push(m1);
+    }
+    var value = m[2] ? m[2].replace(new RegExp('""', "g"), '"') : m[3];
+    row.push(value);
+  }
+
+  return row;
+};
 
 Csonv.init();
 
